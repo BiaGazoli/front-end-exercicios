ba748b0031963fcf8779084603114afcf5ef4434
BiaGazoli <beatrizbgm@outlook.com>
2025-09-12 20:08:32 -0300

    Update main.js

--- exercicios/estruturas-de-decisao/main.js
+++ exercicios/estruturas-de-decisao/main.js
@@ -1,1 +1,7 @@
-prompt ('Digite um número')
+let numero= prompt('Digite um número')
+
+if (numero>10) {
+    console.log('O número digitado é maior que 10');
+}
+
+prompt('Digite um número com três casa decimais')
