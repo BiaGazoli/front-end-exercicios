d742fb99504fd897d73a7f9ca2a67473fd3fe021
BiaGazoli <beatrizbgm@outlook.com>
2025-09-12 21:21:02 -0300

    Exercicio 3

---
+++ exercicios/estruturas-de-decisao/exercicio3.js
@@ -0,0 +1,9 @@
+let senha= prompt('Digite uma senha, apenas com letras')
+
+if (senha!='bolsonaro condenado') {
+    console.log('Senha não salva');
+}   
+
+if (senha='bolsonaro condenado') {
+    console.log('Senha perfeita!');
+}  
