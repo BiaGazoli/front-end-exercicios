29d9f7dab15a426315e7faae0eb6d3124dc0ef9f
BiaGazoli <beatrizbgm@outlook.com>
2025-09-15 20:05:33 -0300

    Exercício 5

---
+++ exercicios/estruturas-de-decisao/exercicio5.js
@@ -0,0 +1,27 @@
+let experiencia= prompt('Qual sua experiência profissional?')
+
+if (experiencia<5) {
+    console.log(10);
+}
+
+if (experiencia>5) {
+    console.log(20);
+}
+
+let formacao= prompt('Qual sua formação acadêmica')
+
+if (formacao=='Superior') {
+    console.log(10);
+}
+
+if (formacao=='Pós graduação') {
+    console.log(20);
+}
+
+if (formacao=='Mestrado') {
+    console.log(30);
+}
+
+if (formacao=='Doutorado') {
+    console.log(40);
+}
