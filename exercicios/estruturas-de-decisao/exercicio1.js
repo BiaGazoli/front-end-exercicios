9043e9aac78c377471cbb3c4e2a7cef2853bfdff
BiaGazoli <beatrizbgm@outlook.com>
2025-09-15 21:40:07 -0300

    resollção

--- exercicios/estruturas-de-decisao/exercicio1.js
+++ exercicios/estruturas-de-decisao/exercicio1.js
@@ -2,11 +2,5 @@ let numero= prompt('Digite um número')
 
 if (numero>10) {
     console.log('O número digitado é maior que 10');
+} else console.log('O número digitado é menor que 10');    
 }
-
-if (numero<10) {
-    console.log('O número digitado é menor que 10');    
-}
-
-prompt('Digite um número com três casa decimais')
-
