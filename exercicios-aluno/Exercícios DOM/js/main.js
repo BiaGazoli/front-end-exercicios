159cc3814241fe99a788e2c4df30537705543bc1
BiaGazoli <beatrizbgm@outlook.com>
2025-09-25 21:46:25 -0300

    primeira parte

---
+++ exercicios-aluno/Exercícios DOM/js/main.js
@@ -0,0 +1,8 @@
+let elemento = document.getElementById('o-cortico');
+console.log (elemento);
+
+let ancora = document.getElementsByTagName('a');
+console.log (ancora);
+
+let vidasSecas = document.querySelector('#vidas-secas');
+console.log (vidasSecas);
