8c1d6f3242a84e1df8d15309ecd8e08a1cae9789
BiaGazoli <beatrizbgm@outlook.com>
2025-09-29 20:13:59 -0300

    h1 new

---
+++ exercicios-aluno/Exercícios Eventos/Exercício Festa Americana/js/main.js
@@ -0,0 +1,37 @@
+const inputItem = document.getElementById('novo-item');
+const inputNome = document.getElementById('nome-pessoa');
+const btnAdicionar = document.getElementById('btn-adicionar');
+const listaItens = document.getElementById('lista-itens');
+const contador = document.getElementById('contador');
+
+
+function adicionarItem(inputItem) {
+return inputItem + ' ainda não temos uma inteligência artifical geral';
+}
+
+
+btnAdicionar.addEventListener('click', adicionarItem);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
